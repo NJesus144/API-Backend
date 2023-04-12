46713739ce55ad018074e04b77babb0d56032944
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-04-11 21:25:09 -0300

    Criado LikeNews, alterando array no banco de dados

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -10,7 +10,8 @@ import {
   byUser,
   update,
   erase,
-} from "../controllers/news.Controller.js";
+  likeNews,
+} from "../controllers/news.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 router.post("/", authMiddleware, create);
@@ -21,5 +22,6 @@ router.get("/byUser", authMiddleware, byUser);
 router.get("/:id", authMiddleware, findById);
 router.patch("/:id", authMiddleware, update);
 router.delete("/:id", authMiddleware, erase);
+router.patch("/like/:id", authMiddleware, likeNews);
 
 export default router;
